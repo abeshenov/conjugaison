b7539780e07626bc9a5745594f55fba71550cf7f
Alexey Beshenov <abeshenov@pasteursoft.com>
2022-11-13 22:08:17 -0600

    Formatting

--- service/settings.gradle.kts
+++ service/settings.gradle.kts
@@ -1,9 +1,9 @@
 rootProject.name = "conjugaison"
 
 pluginManagement {
-  repositories {
-    maven { url = uri("https://repo.spring.io/milestone") }
-    maven { url = uri("https://repo.spring.io/snapshot") }
-    gradlePluginPortal()
-  }
+    repositories {
+        maven { url = uri("https://repo.spring.io/milestone") }
+        maven { url = uri("https://repo.spring.io/snapshot") }
+        gradlePluginPortal()
+    }
 }
